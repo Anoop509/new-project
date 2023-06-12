a5dd56ec11c51e92caf7b3d3fc053ab888920632
Anoop kushwaha <anoopkushwaha509@gmail.com>
2023-06-12 13:54:23 -0400

    5h print

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("i will get a good job soon");
+        System.out.println("this is a change");
+        System.out.println("hello yash");
+    }
+}
